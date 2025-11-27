eadde2adca56731d581002286341f8f4df5b2342
Er. Vineet Rajpal <rajpalvineet800@gmail.com>
2025-11-28 02:33:23 +0530

    bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,13 @@
-import {asyncHandler} from  "../utils/asynchandler.js"
+import {asyncHandler} from  "../utils/asynchandler.js";
+import {User} from "../models/user.model.js";
 const registerUser=asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+    // get user from frontend
+    // validation - not empty
+    // check if user already exists : username , email
+    // check for images  , check for avatar
+    
+
+
+    
 })
 export {registerUser}
